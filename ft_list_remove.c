68f5fb5698d7d625908007acc7bdbb19c40b0fe3
Alki Leka <aleka@c1r4s3.tirana.42.school>
2026-05-07 17:34:05 +0200

    wtf

--- ft_list_remove.c
+++ ft_list_remove.c
@@ -71,25 +71,25 @@
 // 	}
 // }
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
-{
-	t_list	*temp;
+// void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+// {
+// 	t_list	*temp;
+// 	while ((*begin_list) && cmp((*begin_list)->data, data_ref))
+// 	{
+// 		temp = *begin_list;
+// 		*begin_list = (*begin_list)->next;
+// 		free(temp);
+// 	}
+// 	while (*begin_list && (*begin_list)->next)
+// 	{
+// 		if (cmp((*begin_list)->next->data, data_ref) == 0)
+// 		{
+// 			temp = (*begin_list)->next;
+// 			(*begin_list)->next = temp->next;
+// 			free(temp);
+// 		}
+// 		else
+// 			(*begin_list) = (*begin_list)->next;
+// 	}
+// }
 
-	while ((*begin_list) && cmp((*begin_list)->data, data_ref))
-	{
-		temp = *begin_list;
-		*begin_list = (*begin_list)->next;
-		free(temp);
-	}
-	while (*begin_list && (*begin_list)->next)
-	{
-		if (cmp((*begin_list)->next->data, data_ref) == 0)
-		{
-			temp = (*begin_list)->next;
-			(*begin_list)->next = temp->next;
-			free(temp);
-		}
-		else
-			(*begin_list) = (*begin_list)->next;
-	}
-}
